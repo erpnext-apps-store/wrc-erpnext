c848522a384a2c5b78f457c7eeb7155bb88701db
Mangesh-Khairnar <mkhairnar10@gmail.com>
2019-09-16 15:46:09 +0530

    feat: generate vendor payments eft file

---
+++ wrc_erpnext/wrc_erpnext/vendor_payments.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2019, Frappe and contributors
+# For license information, please see license.txt
+
+
+from __future__ import unicode_literals
+import frappe
+from frappe import _
+from collections import OrderedDict
+from wrc_erpnext.wrc_erpnext.payments_integration import execute, generate_file_and_attach_to_doctype
+from frappe.utils import getdate
+
+@frappe.whitelist()
+def generate_report(name):
+	data, file_name = create_eft_file(name)
+	return generate_file_and_attach_to_doctype(file_name, data, 'Payment Order', name)
+
+def create_eft_file(name):
+	''' generates a file for eft transactions based on kundu pei system for processing payment order '''
+
+	payment_order = frappe.get_doc("Payment Order", name)
+
+	if not payment_order.company_bank_account:
+		frappe.throw(_('Company Bank Account has to be mentioned'))
+	# client bank account
+	bank_account = frappe.get_doc("Bank Account", payment_order.company_bank_account)
+
+	trace_record = OrderedDict(
+		account_type = [bank_account, 'bsb_no', 7, 'right', '0'],
+		account_number = [bank_account, 'bank_account_no', 12, 'right', '0']
+	)
+	trace_record = execute(trace_record)
+
+	file_name = 'sample_name.txt'
+	header = get_header_row(payment_order, bank_account)
+	detail = []
+	for ref_doc in payment_order.get("references"):
+		detail.append(get_detail_row(ref_doc, trace_record, bank_account)) 
+
+	trailer = get_trailer_row(payment_order, bank_account)
+	detail_records = "\n".join(detail)
+
+	return "\n".join([header, detail_records, trailer]), file_name
+
+def get_header_row(payment_order, bank_account):
+
+	date = getdate().strftime('%Y%m%d') 
+
+	sequence_no = '01'
+
+	header_row = OrderedDict(
+		record_type=['0', '', 1],
+		blank_1=['', '',17],
+		sequence_no=[sequence_no, '', 2, 'right', '0'],
+		Bank=[bank_account, 'bank', 3],
+		blank_2=['', '', 7],
+		user_name=[bank_account, 'client_name', 26, 'left', ' '],
+		user_id=[bank_account, 'client_code', 6, 'right', '0'],
+		entry_description=['PAYMENT ENTRY', '', 12, 'left', ' '],
+		processing_date=[date, '', 8],
+		blank_3=['', '', 50]
+	)
+	return execute(header_row)
+
+def get_trailer_row(payment_order, bank_account):
+	no_of_records = len(payment_order.get("references"))
+	total_amount = sum(entry.get("amount") for entry in payment_order.get("references"))
+
+	trailer_row = OrderedDict(
+		record_type=['7', '', 1],
+		bsb_number=[bank_account, 'bsb_no', 7],
+		blank_1=['', '', 12],
+		net_amount=[total_amount, '', 10, 'right', '0'],
+		total_credit=[total_amount, '', 10, 'right', '0'],
+		total_debit=[total_amount, '', 10, 'right', '0'],
+		blank_2=['', '', 24],
+		user_records=[no_of_records, '', 6],
+		blank_3=['', '', 52]
+	)
+	return execute(trailer_row)
+
+def get_detail_row(ref_doc, trace_detail, bank_account):
+
+	vendor_bank_account = frappe.get_doc('Bank Account', ref_doc.bank_account)
+
+	account_detail = OrderedDict(
+		account_type = [vendor_bank_account, 'account_type', 3, 'right', '0'],
+		account_number = [vendor_bank_account, 'bank_account_no', 12, 'right', '0']
+	)
+	account_detail = execute(account_detail)
+
+	reference_doc = frappe.get_doc(ref_doc.reference_doctype, ref_doc.reference_name)
+
+	detail_row = OrderedDict(
+		record_type=['1', '', 1],
+		bsb_number=[vendor_bank_account,'bsb_no', 7],
+		vendor_account=[account_detail, '', 15],
+		indicator=[' ', '', 1],
+		transaction_code=['53', '', 2],
+		amount=[reference_doc, 'grand_total', 10, 'right', '0'],
+		payment_to=[ref_doc, 'supplier', 32, 'left', ' '],
+		lodgment_reference=[reference_doc, 'name', 18, 'left', ' '],
+		trace_record=[trace_detail, '', 22],
+		remitter_name=[bank_account, 'client_name', 16, 'left', ' '],
+		withholding_tax=['', '', 8, 'right', '0']
+	)
+
+	return execute(detail_row)
